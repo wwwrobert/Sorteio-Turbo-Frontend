76792475d844693eae593ac5de1055d6f326c387
wwwrobert <robertito.a156@gmail.com>
2024-02-14 14:52:54 -0300

    deploy vercel

--- interfaces/config.js
+++ interfaces/config.js
@@ -1,5 +1,5 @@
-const Url = 'https://sorteio-turbo-backend.onrender.com'; 
+const Url = 'https://sorteio-turbo-backend.vercel.app'; 
 
 // http://localhost:3000
-// https://sorteio-turbo-backend.onrender.com
+// https://sorteio-turbo-backend.vercel.app
 export { Url };
